2a87444c9877c0567cd916ce4ce16a36bfa0ec0a
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-04 16:25:42 +0700

    6320502991_1_5

--- 6320502991_1_5.cpp
+++ 6320502991_1_5.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 int main()
 {
-    int N,i;
+    int N,i,k=0;
     cin>>N;
-    int n[N];
+    int n[N],max_=0;
     for(i=0;i<N;i++)
-        cin>>n[N]
+        cin>>n[i];
+    max_=n[0];
+    for(i=0;i<N;i++)
+    {
+        if(max_<n[i])
+        {
+            max_=n[i];
+            k=i;
+        }
+    }
+    cout<<k+1<<" "<<max_;
 }

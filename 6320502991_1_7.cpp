11f59a808c950fb0d96eb333c7bc764977d6c0de
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-04 17:48:58 +0700

    6320502991_1_7

--- 6320502991_1_7.cpp
+++ 6320502991_1_7.cpp
@@ -5,20 +5,21 @@ int main()
 {
     double n;
     cin>>n;
-    if((floor(n)>=0&&floor(n)<=23)&&((n-floor(n))*100>=0&&(n-floor(n))*100<=59))
+    if((floor(n)>=0&&floor(n)<=24)&&((n-floor(n))*100>=0&&(n-floor(n))*100<=59))
     {
-      if(n>=0&&n<=11.59)
+      if(n>=0&&n<=12)
     {
         cout<<floor(n)<<":"<<(n-floor(n))*100;
         if((n-floor(n))*100==0)
             cout<<"0";
         cout<<" a.m.";
+
     }
-    else if(n>=12&&n<=23.59)
+    else if(n>=12&&n<=24)
     {
-        if(n>=12&&n<13)
+        if(n==24)
         {
-           cout<<floor(n)<<":"<<(n-floor(n))*100;
+           cout<<floor(n)-24<<":"<<(n-floor(n))*100;
         if((n-floor(n))*100==0)
             cout<<"0";
         cout<<" p.m.";

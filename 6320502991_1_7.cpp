59164e458b5de62e535e7067195ccf08c93f4a77
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-04 18:02:00 +0700

    6320502991_1_7

--- 6320502991_1_7.cpp
+++ 6320502991_1_7.cpp
@@ -7,34 +7,41 @@ int main()
     cin>>n;
     if((floor(n)>=0&&floor(n)<=24)&&((n-floor(n))*100>=0&&(n-floor(n))*100<=59))
     {
-      if(n>=0&&n<=12)
+      if(n>=0&&n<=11)
     {
         cout<<floor(n)<<":"<<(n-floor(n))*100;
         if((n-floor(n))*100==0)
             cout<<"0";
         cout<<" a.m.";
-
     }
-    else if(n>=12&&n<=24)
+    else if(n>=12&&n<13)
     {
-        if(n==24)
-        {
-           cout<<floor(n)-24<<":"<<(n-floor(n))*100;
+        cout<<floor(n)<<":"<<(n-floor(n))*100;
         if((n-floor(n))*100==0)
             cout<<"0";
-        cout<<" p.m.";
-        }
-        else if(n>=13)
+        cout<<" noon";
+    }
+    else if(n>=13&&n<=23)
+    {
+        if(n>=13)
         {
             cout<<floor(n)-12<<":"<<(n-floor(n))*100;
+        }
         if((n-floor(n))*100==0)
+        {
             cout<<"0";
         cout<<" p.m.";
         }
     }
-    else
-        cout<<"Error";
+    else if(n>=24&&n<25)
+    {
+        if(n==24)
+           cout<<floor(n)-12<<":"<<(n-floor(n))*100;
+        if((n-floor(n))*100==0)
+        {
+            cout<<"0";
+        cout<<" midnight";
+        }
     }
-    else
-        cout<<"Error";
+    }
 }

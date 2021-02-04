ea48711dba732b3fc42a7de1feb90493a843ba92
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-04 16:47:41 +0700

    6320502991_1_7

---
+++ 6320502991_1_7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double n;
+    cin>>n;
+    if(n>=0&&n<=11.59)
+    {
+        cout<<floor(n)<<":"<<(n-floor(n))*100;
+        if((n-floor(n))*100==0)
+            cout<<"0";
+        cout<<" a.m.";
+    }
+
+    else if(n>=12&&n<=23.59)
+    {
+        cout<<floor(n)<<":"<<(n-floor(n))*100;
+        if((n-floor(n))*100==0)
+            cout<<"0";
+        cout<<" p.m.";
+    }
+
+    else
+        cout<<"Error";
+}

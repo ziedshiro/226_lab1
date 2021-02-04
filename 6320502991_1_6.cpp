8d5497c0b2f39648d1b012ec57091247dc2ad3d9
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-04 17:07:05 +0700

    6320502991_1_6

--- 6320502991_1_6.cpp
+++ 6320502991_1_6.cpp
@@ -2,12 +2,29 @@
 using namespace std;
 int main()
 {
-    int n,m,t,y
+    int n,m,y;
+    long t;
     char x;
     cin>>n>>m>>t;
     cin>>x;
+    y=t;
     do
     {
-        if
-    }while(y!=t)
+        if(y>0)
+        {
+            y-=n;
+            if(y>0)
+                y-=m;
+            else
+            {
+                cout<<"L";break;
+            }
+
+        }
+        else
+        {
+           cout<<"R";break;
+        }
+
+    }while(1);
 }
